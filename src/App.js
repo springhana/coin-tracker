62e277252a6f144e3c794b6d3cf9a6d3ed12ce3f
springhana <xksxksanfro7@naver.com>
2023-03-11 06:06:44 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import styles from "./App.module.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [USDChange, setUSDChange] = useState("");
+  const [myMoney, seyMyMoney] = useState(0);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const onChange = (event) => {
+    setUSDChange(event.target.value);
+  };
+
+  const CoinPrice = (event) => {
+    seyMyMoney(event.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.header}>
+      <h1 className={styles.title}>
+        The Coins! ({loading ? "" : coins.length})
+      </h1>
+
+      <div className={styles.contain}>
+        <div className={styles.change}>
+          <input
+            onChange={onChange}
+            type="number"
+            placeholder="How many daller"
+            value={USDChange}
+          ></input>
+          <h1 className={styles.USDPrice}>
+            {(USDChange * 0.000045).toFixed(6)} BTC
+          </h1>
+        </div>
+
+        {loading ? (
+          <strong>Loading...</strong>
+        ) : (
+          <select onChange={CoinPrice} className={styles.select}>
+            <option>Select coin</option>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.quotes.USD.price}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
+              </option>
+            ))}
+          </select>
+        )}
+
+        <div className={styles.BTC}>
+          You can buy <b />{" "}
+          <i className={styles.BTC_price}>{USDChange / myMoney}.</i>
+        </div>
+      </div>
     </div>
   );
 }
